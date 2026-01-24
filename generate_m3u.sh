f923c6c8336c60a8ff0c79acd709a2e1b48fbabd
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2026-01-24 15:51:01 +0530

    Update generate_m3u.sh

--- generate_m3u.sh
+++ generate_m3u.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 PLAYLIST="/app/playlist.m3u"
-
-BASE_URL="https://cloudflare-managed-url"
+BASE_URL="https://candles-however-five-trademark.trycloudflare.com"
 
 echo "#EXTM3U" > $PLAYLIST
 
-jq -c '.[]' /app/channels.json | while read ch; do
+jq -c '.channels[]' /app/channels.json | while read ch; do
   NAME=$(echo "$ch" | jq -r '.name')
 
   echo "#EXTINF:-1 tvg-id=\"$NAME\" tvg-name=\"$NAME\" group-title=\"Live\",$NAME" >> $PLAYLIST
-  echo "$BASE_URL/hls/$NAME/stream.m3u8" >> $PLAYLIST
+  echo "$BASE_URL/hls/$NAME/index.m3u8" >> $PLAYLIST
 done
