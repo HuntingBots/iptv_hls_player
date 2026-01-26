009452ce9c63dc8a6b7473e0dec59e5b12afded3
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2026-01-26 19:06:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-set -e
-
-echo "[+] IPTV system starting..."
-
-mkdir -p /app/streams /app/output /app/logs
-
 nginx
-
-sleep 2
-
-bash /app/nm3u8dl_worker.sh &
-
-while true; do
-    bash /app/generate_m3u.sh
-    sleep 20
-done
+bash /app/worker.sh
